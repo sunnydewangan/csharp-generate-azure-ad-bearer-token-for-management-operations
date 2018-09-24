111f60c4120842f9f6d18aee86d677cec9d89cfd
Sunny Dewangan <sadewa@microsoft.com>
2018-09-24 10:32:03 +0530

    Add project files.

---
+++ Generate Azure AD Bearer Token/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generate_Azure_AD_Bearer_Token
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
